d36c779a39de691ff592937a2f6ade0959c0e4ea
KotaAoyama <hongcha811@gmail.com>
2020-09-22 21:57:11 +0900

    Add HomeControllers, LoginController and SignupController

---
+++ project1/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/SignupController.java
@@ -0,0 +1,42 @@
+package com.udacity.jwdnd.course1.cloudstorage.controller;
+
+import com.udacity.jwdnd.course1.cloudstorage.model.User;
+import com.udacity.jwdnd.course1.cloudstorage.service.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/signup")
+public class SignupController {
+
+    private final UserService userService;
+
+    public SignupController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping()
+    public String signupView() {
+        return "signup";
+    }
+
+    @PostMapping()
+    public String signupUser(@ModelAttribute User user, Model model) {
+        String signupError = null;
+        if (!userService.isUserNameAvailable(user.getUserName())) {
+            signupError = "The userName already exists.";
+        }
+        if (signupError == null) {
+            System.out.println("signupSuccess!");
+            model.addAttribute("signupSccess", true);
+        } else {
+            System.out.println("signupError!");
+            model.addAttribute("signupError", signupError);
+        }
+        return "signup";
+    }
+}
