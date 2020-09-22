7540b798fcdd9a45787d8b8e845293afc34827f7
KotaAoyama <hongcha811@gmail.com>
2020-09-22 12:31:26 +0900

    add security config, mapper, user model and AuthenticationService

---
+++ project1/src/main/java/com/udacity/jwdnd/course1/cloudstorage/config/SecurityConfig.java
@@ -0,0 +1,9 @@
+package com.udacity.jwdnd.course1.cloudstorage.config;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+
+}
