efea2fc1ca5554d77476c45d7de26980a16f57ea
KotaAoyama <hongcha811@gmail.com>
2020-09-22 21:59:45 +0900

    Add UserService

--- project1/src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/UserMapper.java
+++ project1/src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/UserMapper.java
@@ -1,7 +1,9 @@
 package com.udacity.jwdnd.course1.cloudstorage.mapper;
 
 import com.udacity.jwdnd.course1.cloudstorage.model.User;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Select;
 
 @Mapper
@@ -9,4 +11,8 @@ public interface UserMapper {
 
     @Select("SELECT * FROM USERS WHERE username = #{userName}")
     User getUser(String userName);
+
+    @Insert("INSERT INTO USERS (username, salt, password, firstname, lastname) VALUES (#{userName}, #{password}, #{salt}, #{firstName}, #{lastName})")
+    @Options(useGeneratedKeys = true, keyProperty = "userId")
+    int insert(User user);
 }
