de87b676934ae450174413bfaf90217aa2a491ec
KotaAoyama <hongcha811@gmail.com>
2021-03-10 23:55:59 +0900

    feat: (wip) convert pricing-service, from rest to microservice

---
+++ project2_vehicles_api/pricing-service/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO price (id, currency, price, vehicle_id) VALUES (1, 'yen', 100000000, 1);
+INSERT INTO price (id, currency, price, vehicle_id) VALUES (2, 'dollar', 10000, 2);
+INSERT INTO price (id, currency, price, vehicle_id) VALUES (3, 'euro', 300, 3);
